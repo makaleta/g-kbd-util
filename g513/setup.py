d5c919be07ebec1f92178a33614a090d045e119e
Marek Kaleta <mk@m1k.cz>
2023-11-22 15:44:03 +0100

    black

--- g513/setup.py
+++ g513/setup.py
@@ -12,59 +12,63 @@ COLOR_SYS = "a00000"
 
 MEM_TOTAL = int(os.popen("vmstat -SM -s | head -1 | sed 's/[^0-9]//g'").read().strip())
 
+
 def g513_led(command):
-  os.system(f"g513-led {command}")
+    os.system(f"g513-led {command}")
+
 
 def numlock_status():
-  num_lock_status = os.popen("xset -q | sed -n 's/^.*Num Lock:\s*\(\S*\).*$/\1/p'").read().strip()
-  if num_lock_status == "on":
-    g513_led("numeric 999999")
-  else:
-    g513_led("numeric 000000")
+    num_lock_status = (
+        os.popen("xset -q | sed -n 's/^.*Num Lock:\s*\(\S*\).*$/\1/p'").read().strip()
+    )
+    if num_lock_status == "on":
+        g513_led("numeric 999999")
+    else:
+        g513_led("numeric 000000")
+
 
 def sysstats():
-  sysstat = os.popen("vmstat -y -SM | tail -n 1").read().strip().split()
-  mem_free = int(sysstat[3])
-  mem_cache = int(sysstat[5])
-  sys_sys = int(sysstat[13])
-  sys_idle = int(sysstat[14])
-  sys_sys_blocks = int((sys_sys / 100) * 9)
-  sys_used_blocks = 9 - int((sys_idle / 100) * 9)
-  mem_nonused_blocks = int(((mem_free+mem_cache) / MEM_TOTAL) * 5)
-  mem_cache_blocks = int((mem_cache / MEM_TOTAL) * 5)
-  index = 1
-  draw = 0
-  while draw < sys_sys_blocks:
-    g513_led(f"-kn f{index} {COLOR_SYS}")
-    index += 1
-    draw += 1
-  draw = 0
-  while draw < sys_used_blocks:
-    g513_led(f"-kn f{index} {COLOR_USED}")
-    index += 1
-    draw += 1
-  while index <= 8:
-    g513_led(f"-kn f{index} {COLOR}")
-    index += 1
-  draw = 0
-  while draw < mem_cache_blocks:
-    g513_led(f"-kn f{index} {COLOR_CACHE}")
-    index += 1
-    draw += 1
-  while draw < mem_nonused_blocks:
-    g513_led(f"-kn f{index} {COLOR}")
-    index += 1
-    draw += 1
-  while index <= 12:
-    g513_led(f"-kn f{index} {COLOR_USED}")
-    index += 1
-  g513_led("-c")
+    sysstat = os.popen("vmstat -y -SM | tail -n 1").read().strip().split()
+    mem_free = int(sysstat[3])
+    mem_cache = int(sysstat[5])
+    sys_sys = int(sysstat[13])
+    sys_idle = int(sysstat[14])
+    sys_sys_blocks = int((sys_sys / 100) * 9)
+    sys_used_blocks = 9 - int((sys_idle / 100) * 9)
+    mem_nonused_blocks = int(((mem_free + mem_cache) / MEM_TOTAL) * 5)
+    mem_cache_blocks = int((mem_cache / MEM_TOTAL) * 5)
+    index = 1
+    draw = 0
+    while draw < sys_sys_blocks:
+        g513_led(f"-kn f{index} {COLOR_SYS}")
+        index += 1
+        draw += 1
+    draw = 0
+    while draw < sys_used_blocks:
+        g513_led(f"-kn f{index} {COLOR_USED}")
+        index += 1
+        draw += 1
+    while index <= 8:
+        g513_led(f"-kn f{index} {COLOR}")
+        index += 1
+    draw = 0
+    while draw < mem_cache_blocks:
+        g513_led(f"-kn f{index} {COLOR_CACHE}")
+        index += 1
+        draw += 1
+    while draw < mem_nonused_blocks:
+        g513_led(f"-kn f{index} {COLOR}")
+        index += 1
+        draw += 1
+    while index <= 12:
+        g513_led(f"-kn f{index} {COLOR_USED}")
+        index += 1
+    g513_led("-c")
 
-  
 
 g513_led(f"-a {COLOR}")
 
 while True:
-  sysstats()
-  sleep(.7)
-  pass
+    sysstats()
+    sleep(0.7)
+    pass
--- install.py
+++ install.py
@@ -4,6 +4,7 @@ import os
 import sys
 import argparse
 
+
 def find_service_file(directory: str) -> str:
     directory_path = os.path.abspath(directory)
     if not os.path.exists(directory_path):
@@ -19,7 +20,8 @@ def find_service_file(directory: str) -> str:
         sys.exit(1)
     return service_file
 
-def install_systemd_service(directory:str, enable: bool):
+
+def install_systemd_service(directory: str, enable: bool):
     service_file = find_service_file(directory)
     service_name = os.path.splitext(os.path.basename(service_file))[0]
     systemd_path = f"/etc/systemd/system/{service_name}.service"
@@ -36,6 +38,7 @@ def install_systemd_service(directory:str, enable: bool):
         print(f"Systemd service '{service_name}' enabled, and started successfully.")
     sys.exit(0)
 
+
 def uninstall_systemd_service(directory: str):
     service_file = find_service_file(directory)
     service_name = os.path.splitext(os.path.basename(service_file))[0]
@@ -48,10 +51,12 @@ def uninstall_systemd_service(directory: str):
     print(f"Systemd service '{service_name}' uninstalled successfully.")
     sys.exit(0)
 
+
 def issudo():
     if os.geteuid() != 0:
         print("This script must be run as root.")
         sys.exit(1)
+
 
 def ensure_810_led_installed():
     if not os.path.exists("/lib/systemd/system/g810-led-reboot.service"):
@@ -59,11 +64,18 @@ def ensure_810_led_installed():
         print("sudo apt install g810-led")
         sys.exit(1)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("directory", help="Path to the directory containing the service file")
-    parser.add_argument("--uninstall", action="store_true", help="Uninstall the service")
-    parser.add_argument("--enable", action="store_true", help="Start the service on boot")
+    parser.add_argument(
+        "directory", help="Path to the directory containing the service file"
+    )
+    parser.add_argument(
+        "--uninstall", action="store_true", help="Uninstall the service"
+    )
+    parser.add_argument(
+        "--enable", action="store_true", help="Start the service on boot"
+    )
     args = parser.parse_args()
     issudo()
 
